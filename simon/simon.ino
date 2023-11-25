5fbc704572daf6b5b1133d3b7e3818a445c990c8
Jesús Barrera <jesus.ba015@gmail.com>
2023-11-24 21:32:58 -0600

    Add buzzer

--- simon/simon.ino
+++ simon/simon.ino
@@ -1,6 +1,8 @@
+#include "pitches.h"
+
 #define MAX_SEQUENCE_LENGTH 255
 
-#define NO_INPUT 0
+#define NO_INPUT -1
 
 #define INPUT_DELAY 100
 
@@ -10,15 +12,18 @@
 // Number of milliseconds each LED is on when playing the sequence.
 #define PLAY_DELAY 250
 
+#define BUZZER_PIN 8
+
 // Checks the pushbuttons' state and returns the first that is
 // pressed, or NO_INPUT if none.
-byte readInput();
+int readInput();
 
 // Plays the current sequence.
 void playSequence();
 
 // Pins connected to the LEDs/pushbuttons
-byte pins[] = {2, 3, 4};
+byte pins[] = {2, 3, 4, 5};
+byte pinsNotes[] = {NOTE_C3, NOTE_D3, NOTE_E3, NOTE_F3};
 
 byte sequence[MAX_SEQUENCE_LENGTH];
 byte sequenceLength;
@@ -33,26 +38,33 @@ void setup() {
 }
 
 void loop() {
-  static byte input;
+  static int input;
 
   if (asserts < sequenceLength) {
-    if (input = readInput()) {
-      do {
-        delay(INPUT_DELAY);
-      } while (readInput()); // wait for button release
-
+    input = readInput();
+    
+    if (input != NO_INPUT) {
       if (input == sequence[asserts]) {
         asserts++;
       } else {
         Serial.println("Game Over! Press restart.");
         while (true);
       }
+
+      tone(BUZZER_PIN, pinsNotes[input]);
+
+      // wait for button release
+      do {
+        delay(INPUT_DELAY);
+      } while (readInput() == input);
+
+      noTone(BUZZER_PIN);
     }
   } else {
     delay(PAUSE_DELAY);
 
     // add random item to the sequence
-    sequence[sequenceLength++] = pins[ random(0, sizeof(pins)) ];
+    sequence[sequenceLength++] = random(0, sizeof(pins));
     playSequence();
     asserts = 0;
   }
@@ -70,9 +82,15 @@ void playSequence() {
   // Play sequence
   i = 0;
   while (true) {
-    digitalWrite(sequence[i], LOW);
-    delay(PLAY_DELAY);
-    digitalWrite(sequence[i], HIGH);
+    // play note and turn LED on
+    tone(BUZZER_PIN, pinsNotes[sequence[i]]);
+    digitalWrite(pins[sequence[i]], LOW);
+    
+    delay(PLAY_DELAY); 
+
+    // buzzer and LED off
+    noTone(BUZZER_PIN);
+    digitalWrite(pins[sequence[i]], HIGH);
 
     if (++i < sequenceLength) {
       delay(PLAY_DELAY);
@@ -87,10 +105,10 @@ void playSequence() {
   }
 }
 
-byte readInput() {
+int readInput() {
   for (int i = 0; i < sizeof(pins); i++) {
     if (digitalRead(pins[i]) == LOW) {
-      return pins[i];
+      return i;
     }
   }
 
